c0cba09c8dfc90c63dfa0447ebaca04fb5bc21d7
Tarunsai Simhadri <tarunsimhadri23@gmil.com>
2024-07-23 22:00:33 +0530

    [Tarunsai] Add: function to validate last name

--- user_registration.py
+++ user_registration.py
@@ -31,6 +31,25 @@ def validate_first_name(first_name):
         return False
     return True
 
+def validate_last_name(last_name):
+
+    """
+    description:
+        Validates the last name.
+
+    parameters:
+        last_name: The last name to validate.
+
+    Returns:
+        True if the last name is valid, False otherwise.
+    """
+      
+    if not last_name[0].isupper():
+        return False
+    if len(last_name) < 3:
+        return False
+    return True
+
 
 def main():
 
@@ -42,6 +61,15 @@ def main():
             break
         else:
             logger.info("Invalid first name. Please try again.")
+
+    while True:
+        last_name = input("Enter a valid last name: ")
+        logger = get_logger("validate_last_name")
+        if validate_last_name(last_name):
+            logger.info("last_name registered successfully")
+            break
+        else:
+            logger.info("Invalid last name. Please try again.")
 
 
 if __name__ == '__main__':
