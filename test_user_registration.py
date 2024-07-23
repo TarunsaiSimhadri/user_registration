e41a59bff56e2448dfeb63a54c44aecb04cd859b
Tarunsai Simhadri <tarunsimhadri23@gmil.com>
2024-07-23 22:58:09 +0530

    [Tarunsai] Add: test cases for UC_3

--- test_user_registration.py
+++ test_user_registration.py
@@ -23,6 +23,26 @@ class TestUserRegistration(unittest.TestCase):
         self.assertTrue(user_registration.validate_last_name("Tarun"))
         self.assertFalse(user_registration.validate_last_name("tarun"))
 
+    def test_validate_email(self):
+        self.assertTrue(user_registration.validate_email("abc@yahoo.com"))
+        self.assertTrue(user_registration.validate_email("abc-100@yahoo.com"))
+        self.assertTrue(user_registration.validate_email("abc.100@yahoo.com"))
+        self.assertTrue(user_registration.validate_email("abc111@abc.com"))
+        self.assertTrue(user_registration.validate_email("abc111@abc.net"))
+        self.assertTrue(user_registration.validate_email("abc.100@abc.com.au"))
+        self.assertTrue(user_registration.validate_email("abc@1.com"))
+        self.assertTrue(user_registration.validate_email("abc@gmail.com.com"))
+        self.assertTrue(user_registration.validate_email("abc+100@gmail.com"))
+
+        self.assertFalse(user_registration.validate_email("abc"))
+        self.assertFalse(user_registration.validate_email("abc.com.my"))
+        self.assertFalse(user_registration.validate_email("abc123@gmail.a"))
+        self.assertFalse(user_registration.validate_email("abc123@.com"))
+        self.assertFalse(user_registration.validate_email("abc()*@gmail.com"))
+        self.assertFalse(user_registration.validate_email("abc@%*.com"))
+        self.assertFalse(user_registration.validate_email("abc@abc@gmail.com"))
+        self.assertFalse(user_registration.validate_email("abc@gmail.com.1a"))
+
 
 if __name__ == "__main__":
     unittest.main()
